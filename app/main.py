58c11032ffff2252f67f1dd83a50b7076bf4e9ed
zayac880 <zayac880880@mail.ru>
2024-02-13 20:42:51 +0300

    the creation of products and the aggregation of products with tasks has been implemented, details in the code (models, diagrams) have also been corrected

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI, Depends, HTTPException, Path
+from fastapi import FastAPI, Depends, HTTPException, Path, APIRouter
 from sqlalchemy.orm import Session
 from .import models, schemas, crud
 from .database import SessionLocal, engine
 
 app = FastAPI()
+
+task_router = APIRouter(prefix="/shift-tasks", tags=["Tasks"])
+product_router = APIRouter(prefix="/products", tags=["Products"])
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -16,13 +19,13 @@ def get_db():
         db.close()
 
 
-@app.post("/shift-tasks/")
+@task_router.post("/create")
 def post_shift_task(shift_task: schemas.ShiftTaskCreate, db: Session = Depends(get_db)):
     created_task = crud.create_shift_task(shift_task, db)
     return created_task
 
 
-@app.get("/shift-tasks/{task_id}")
+@task_router.get("/get-{task_id}")
 def get_shift_task(task_id: int = Path(..., title="The ID of the shift task to retrieve"),
                     db: Session = Depends(get_db)):
     shift_task = crud.get_shift_task_by_id(db, task_id)
@@ -33,7 +36,7 @@ def get_shift_task(task_id: int = Path(..., title="The ID of the shift task to r
     return shift_task
 
 
-@app.put("/shift-tasks/{task_id}")
+@task_router.put("/update-{task_id}")
 def update_shift_task(task_id: int, shift_task_update: schemas.ShiftTaskUpdate, db: Session = Depends(get_db)):
     updated_task = crud.update_shift_task(db, task_id, shift_task_update)
     if updated_task is None:
@@ -41,6 +44,30 @@ def update_shift_task(task_id: int, shift_task_update: schemas.ShiftTaskUpdate, 
     return updated_task
 
 
-@app.get("/shift-tasks-filter/")
+@task_router.get("/filter")
 def filter_get_shift_tasks(filters: schemas.ShiftTaskFilter = Depends(), db: Session = Depends(get_db)):
     return crud.filter_shift_tasks(db, filters)
+
+
+@product_router.post("/create")
+def add_products_to_batch(
+    products: schemas.ProductListInput,
+    db: Session = Depends(get_db)
+):
+    added_products = crud.add_products(db, products)
+    return added_products
+
+
+@product_router.post("/create-aggregate")
+def aggregate_product(
+    aggregation_input: schemas.AggregationInput,
+    db: Session = Depends(get_db)
+):
+    product_code = crud.aggregate_product(db, aggregation_input)
+    if not product_code:
+        raise HTTPException(status_code=404, detail="Product code not found")
+    return product_code
+
+
+app.include_router(task_router)
+app.include_router(product_router)
